ab0ce29ba42e3d33e11db784ddb396685efd4f8f
Alex Nikitin <olion007@gmail.com>
2018-03-18 22:03:10 +0300

    Add POST "items/add" interface to api

--- app/src/main/java/ru/tux/moneytracker/Api.java
+++ app/src/main/java/ru/tux/moneytracker/Api.java
@@ -3,7 +3,9 @@ package ru.tux.moneytracker;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -14,5 +16,8 @@ public interface Api {
 
     @GET("/items")
     Call<List<Record>> getItems(@Query("type") String type);
+
+    @POST("/items/add")
+    Call<Void> createItem(@Body Record record);
 
 }
