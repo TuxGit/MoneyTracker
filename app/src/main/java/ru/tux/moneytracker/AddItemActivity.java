f3c54a36b2f05cca8bcd71c8103c80f15f6f3d71
Alex Nikitin <olion007@gmail.com>
2018-03-22 21:54:05 +0300

    Add handler for back button on add item screen
    getSupportActionBar().setDisplayHomeAsUpEnabled(true)

--- app/src/main/java/ru/tux/moneytracker/AddItemActivity.java
+++ app/src/main/java/ru/tux/moneytracker/AddItemActivity.java
@@ -8,6 +8,7 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -105,5 +106,16 @@ public class AddItemActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
