0806fe199fc8e68d4377cc2b90d5f8ad8101c7c4
Alex Nikitin <olion007@gmail.com>
2018-03-25 08:11:19 +0300

    Add Auth Activity with google authentication Extract base api url to build settings and changed url from sandbox to "laravel" api backend

--- app/src/main/java/ru/tux/moneytracker/Api.java
+++ app/src/main/java/ru/tux/moneytracker/Api.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
+import ru.tux.moneytracker.Record;
+import ru.tux.moneytracker.api.AuthResult;
 
 /**
  * Created by tux on 18/03/18.
@@ -14,10 +17,16 @@ import retrofit2.http.Query;
 
 public interface Api {
 
-    @GET("/items")
+    @GET("auth")
+    Call<AuthResult> auth(@Query("social_user_id") String userId);
+
+    @GET("items")
     Call<List<Record>> getItems(@Query("type") String type);
 
-    @POST("/items/add")
+    @POST("items/add")
     Call<Void> createItem(@Body Record record);
+
+    @DELETE("items/remove")
+    Call<Void> removeItem(@Query("id") int id);
 
 }
--- app/src/main/java/ru/tux/moneytracker/MainActivity.java
+++ app/src/main/java/ru/tux/moneytracker/MainActivity.java
@@ -35,8 +35,8 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         viewPager = findViewById(R.id.view_pager);
         tabLayout = findViewById(R.id.tab_layout);
 
-        MainPageAdapter adapter = new MainPageAdapter(getSupportFragmentManager(), this);
-        viewPager.setAdapter(adapter);
+        // MainPageAdapter adapter = new MainPageAdapter(getSupportFragmentManager(), this);
+        // viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(this);
 
         tabLayout.setupWithViewPager(viewPager);
@@ -64,39 +64,56 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         // Log.d(TAG, "onCreate");
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // Log.d(TAG, "onStart");
-    }
+    // @Override
+    // protected void onStart() {
+    //     super.onStart();
+    //
+    //     // Log.d(TAG, "onStart");
+    // }
+    //
+    // @Override
+    // protected void onResume() {
+    //     super.onResume();
+    //
+    //     // Log.d(TAG, "onResume");
+    // }
+    //
+    // @Override
+    // protected void onPause() {
+    //     super.onPause();
+    //
+    //     // Log.d(TAG, "onPause");
+    // }
+    //
+    // @Override
+    // protected void onStop() {
+    //     super.onStop();
+    //
+    //     // Log.d(TAG, "onStop");
+    // }
+    //
+    // @Override
+    // protected void onDestroy() {
+    //     super.onDestroy();
+    //
+    //     // Log.d(TAG, "onDestroy");
+    // }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        // Log.d(TAG, "onResume");
+        if (((App) getApplication()).isAuthorized()) {
+            initTabs();
+        } else {
+            Intent intent = new Intent(this, AuthActivity.class);
+            startActivity(intent);
+        }
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        // Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Log.d(TAG, "onDestroy");
+    private void initTabs() {
+        MainPageAdapter adapter = new MainPageAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(adapter);
     }
 
     @Override
